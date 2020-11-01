6326dbc9cae1ba237e2d32c325a457ea00cc4d5b
matsuhaya <matsuhaya007@gmail.com>
2020-11-01 18:50:48 +0900

    Add middleware for logging
    -  log format is LTSV

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type Yamabiko struct {
@@ -12,6 +14,14 @@ type Yamabiko struct {
 
 func main() {
 	e := echo.New()
+
+	logger := middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: logFormat(),
+		Output: os.Stdout,
+	})
+	e.Use(logger)
+	e.Use(middleware.Recover())
+
 	e.POST("/v1/echo", postEcho)
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -22,4 +32,28 @@ func postEcho(c echo.Context) error {
 		return err
 	}
 	return c.JSON(http.StatusOK, y)
+}
+
+func logFormat() string {
+	var format string
+	format += "time:${time_rfc3339}\t"
+	format += "host:${remote_ip}\t"
+	format += "forwardedfor:${header:x-forwarded-for}\t"
+	format += "req:-\t"
+	format += "status:${status}\t"
+	format += "method:${method}\t"
+	format += "uri:${uri}\t"
+	format += "size:${bytes_out}\t"
+	format += "referer:${referer}\t"
+	format += "ua:${user_agent}\t"
+	format += "reqtime_ns:${latency}\t"
+	format += "cache:-\t"
+	format += "runtime:-\t"
+	format += "apptime:-\t"
+	format += "vhost:${host}\t"
+	format += "reqtime_human:${latency_human}\t"
+	format += "x-request-id:${id}\t"
+	format += "host:${host}\n"
+
+	return format
 }
